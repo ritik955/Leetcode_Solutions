e788b5e9868c35fd6110924734a818533e9f1552
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-08-13 13:29:40 +0530

    Time: 3 ms (31.58%), Space: 45 MB (13.68%) - LeetHub

---
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int[] maxDepthAfterSplit(String seq) {
+    int[] maxDepth = new int[seq.length()];
+    int currDepth = 0;
+    for (int i = 0; i < seq.length(); i++) {
+      if (seq.charAt(i) == '(') {
+        currDepth++;
+      }
+      maxDepth[i] = currDepth % 2;
+      if (seq.charAt(i) != '(') {
+        currDepth--;
+      }
+    }
+    return maxDepth;
+  }
+}
